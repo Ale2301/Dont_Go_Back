513fad0172873c40fb087bea4dea9e9080882a1c
Ale2301 <alejo.2001@live.com.ar>
2025-03-03 00:26:34 -0300

    first commit

---
+++ Camera_Movement.gd
@@ -0,0 +1,24 @@
+extends Camera3D
+@export var rotation_speed = 40
+var original_camera_rotation: Vector3
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	original_camera_rotation = rotation_degrees
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	handle_camera_rotation(delta)
+
+func handle_camera_rotation(delta):
+	if Input.is_action_pressed("look_back"):
+		rotation_degrees.y = original_camera_rotation.y - 180
+	elif Input.is_action_pressed("look_right"):
+		rotation_degrees.y = original_camera_rotation.y - 90
+	elif Input.is_action_pressed("look_left"):
+		rotation_degrees.y = original_camera_rotation.y + 90
+	if Input.is_action_pressed("move_forward"):
+		rotation_degrees = original_camera_rotation
---
+++ player.gd
@@ -0,0 +1,32 @@
+extends CharacterBody3D
+
+# How fast the player moves in meters per second.
+@export var speed = 4 #Te ponen
+@export var gravity = 20
+@export var teleport_limit_x = 0.0
+@export var teleport_target_x = 79.24
+
+
+var target_velocity = Vector3.ZERO
+
+
+func _physics_process(delta):
+	var direction = Vector3.ZERO
+
+	if Input.is_action_pressed("move_forward"):
+		direction = -transform.basis.x
+	if direction.length() > 0:
+		direction = direction.normalized()
+		target_velocity = direction * speed
+	
+	if not is_on_floor():
+		target_velocity.y -=gravity*delta
+	else:
+		target_velocity.y = 0
+
+	# Moving the Character
+	velocity = target_velocity
+	move_and_slide()
+	
+	if global_transform.origin.x <= teleport_limit_x:
+		global_transform.origin.x = teleport_target_x
