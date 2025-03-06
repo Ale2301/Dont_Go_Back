2f06d3da7c1fed729ca861e7593ef7a52f1ea071
Ale2301 <alejo.2001@live.com.ar>
2025-03-06 17:42:08 -0300

    feat: signal numbers now are random

--- scripts/main.gd
+++ scripts/main.gd
@@ -4,6 +4,11 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	var player = $Player
+	var label = get_node("Signal 1/SignalText/SubViewport/Label")
+	var label2 = get_node("Signal 5/SignalText/SubViewport/Label")
+	player.connect("change_miles",Callable(label,"_on_change_miles"))
+	player.connect("change_miles",Callable(label2,"_on_change_miles"))
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
